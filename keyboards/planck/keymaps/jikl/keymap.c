78892ff9847e4f5ecfa9cf050d8081c82e8512a1
Jonas Bernoulli <jonas@bernoul.li>
2023-03-24 15:39:36 +0100

    jikl: Use key-overrides instead of tri-layer

--- keyboards/planck/keymaps/jikl/keymap.c
+++ keyboards/planck/keymaps/jikl/keymap.c
@@ -3,18 +3,7 @@
 
 #include QMK_KEYBOARD_H
 
-enum layers {_Q_, _C_, _M_, _B_, _L_, _F_};
-
-layer_state_t layer_state_set_user(layer_state_t state) {
-  return update_tri_layer_state(state, _C_, _M_, _B_);
-}
-
-const key_override_t semicolon = ko_make_basic(MOD_MASK_SHIFT, KC_COMM, KC_SCLN);
-const key_override_t colon     = ko_make_basic(MOD_MASK_SHIFT, KC_DOT,  KC_COLN);
-
-const key_override_t **key_overrides = (const key_override_t *[]){
-    &semicolon, &colon, NULL
-};
+enum layers {_Q_, _L_, _F_};
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -28,14 +17,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | Fn   |      | Bksp | Gui  | Ctrl | Alt  | Alt  | Ctrl | Gui  |      |      | Fn   |
  * `-----------------------------------------------------------------------------------'
- */
-[0] = LAYOUT_planck_grid(
-    KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_ENT,
-    MO(_L_), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SPC,  MO(_L_),
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_TAB,  KC_RSFT,
-    MO(_F_), XXXXXXX, XXXXXXX, KC_LGUI, MO(_C_), MO(_M_), KC_RALT, KC_RCTL, KC_RGUI, XXXXXXX, XXXXXXX, MO(_F_)),
-
-/* ,-----------------------------------------------------------------------------------.
+ *
+ * ,-----------------------------------------------------------------------------------.
  * |      |      |      |      |      |      |      | Home | Up   | End  |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |      |      | Left | Down | Right|      |      |
@@ -45,23 +28,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |      |      |      |      |      |      |      |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
-[_C_] = LAYOUT_planck_grid(
-    _______, C(KC_Q), C(KC_W), C(KC_E), C(KC_R), C(KC_T), C(KC_Y), KC_HOME, KC_UP,      KC_END,     C(KC_P),   C(KC_ENT),
-    _______, C(KC_A), C(KC_S), C(KC_D), C(KC_F), C(KC_G), C(KC_H), KC_LEFT, KC_DOWN,    KC_RGHT,    C(KC_SPC), _______,
-    _______, C(KC_Z), C(KC_X), C(KC_C), C(KC_V), C(KC_B), C(KC_N), KC_BSPC, C(KC_COMM), KC_DEL,     C(KC_TAB), _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______,    _______,    _______,   _______),
-
-[_M_] = LAYOUT_planck_grid(
-    _______, A(KC_Q), A(KC_W), A(KC_E), A(KC_R), A(KC_T), A(KC_Y), C(KC_HOME), C(KC_UP),   C(KC_END),  A(KC_P),   A(KC_ENT),
-    _______, A(KC_A), A(KC_S), A(KC_D), A(KC_F), A(KC_G), A(KC_H), C(KC_LEFT), C(KC_DOWN), C(KC_RGHT), A(KC_SPC), _______,
-    _______, A(KC_Z), A(KC_X), A(KC_C), A(KC_V), A(KC_B), A(KC_N), C(KC_BSPC), A(KC_COMM), C(KC_DEL),  A(KC_TAB), _______,
-    _______, _______, _______, _______, _______, _______, _______, _______,    _______,    _______,    _______,   _______),
-
-[_B_] = LAYOUT_planck_grid(
-    _______, LCA(KC_Q), LCA(KC_W), LCA(KC_E), LCA(KC_R), LCA(KC_T), LCA(KC_Y), A(KC_HOME), A(KC_UP),     A(KC_END),  LCA(KC_P),   LCA(KC_ENT),
-    _______, LCA(KC_A), LCA(KC_S), LCA(KC_D), LCA(KC_F), LCA(KC_G), LCA(KC_H), A(KC_LEFT), A(KC_DOWN),   A(KC_RGHT), LCA(KC_SPC), _______,
-    _______, LCA(KC_Z), LCA(KC_X), LCA(KC_C), LCA(KC_V), LCA(KC_B), LCA(KC_N), A(KC_BSPC), LCA(KC_COMM), A(KC_DEL),  LCA(KC_TAB), _______,
-    _______, _______,   _______,   _______,   _______,   _______,   _______,   _______,    _______,      _______,    _______,     _______),
+[_Q_] = LAYOUT_planck_grid(
+    KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_ENT,
+    MO(_L_), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SPC,  MO(_L_),
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_TAB,  KC_RSFT,
+    MO(_F_), XXXXXXX, XXXXXXX, KC_LGUI, KC_LCTL, KC_LALT, KC_RALT, KC_RCTL, KC_RGUI, XXXXXXX, XXXXXXX, MO(_F_)),
 
 /* ,-----------------------------------------------------------------------------------.
  * |      |   #  |   [  |   '  |   ]  |   %  |   =  |   <  |   ~  |   >  |   ^  |      |
